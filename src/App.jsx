7844a00213ba636fe135a375b822f324f16c911b
Yazan Egbaria <yazanegbaria07@gmail.com>
2024-11-26 17:39:25 +0200

    Finished my redux app

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+import CartContainer from "./components/CartContainer";
+import Navbar from "./components/Navbar";
+import { useDispatch, useSelector } from "react-redux";
+import { calculateTotals } from "./features/cartSlice";
+import Modal from "./components/Modal";
+
+function App() {
+  const dispatch = useDispatch();
+  const { cartItems } = useSelector((state) => state.cart);
+  const { isOpen } = useSelector((state) => state.modal);
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
+  return (
+    <main>
+      {isOpen && <Modal />}
+      <Navbar />
+      <CartContainer />
+    </main>
+  );
+}
+export default App;
